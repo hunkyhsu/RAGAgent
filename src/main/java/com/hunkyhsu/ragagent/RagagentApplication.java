24a35a158748a6d847bd3873a7820c995495a9da
hongkai xv <89715703+VarusXhk@users.noreply.github.com>
2025-12-26 23:12:11 +0800

    initialization of project

---
+++ src/main/java/com/hunkyhsu/ragagent/RagagentApplication.java
@@ -0,0 +1,13 @@
+package com.hunkyhsu.ragagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagagentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hunkyhsu/ragagent/RagagentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunkyhsu.ragagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagagentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
